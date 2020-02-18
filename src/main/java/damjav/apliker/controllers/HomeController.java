6562e186597f8b14e3a362cda3ee3ea72a792aa9
DamJav <damian.garbacz.9@gmail.com>
2020-02-18 19:50:44 +0100

    cd

--- src/main/java/damjav/apliker/controllers/HomeController.java
+++ src/main/java/damjav/apliker/controllers/HomeController.java
@@ -6,11 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/home")
+@RequestMapping("/")
 public class HomeController {
 
     @GetMapping
-    public String showHomePage(){
-        return "home";
+    public String getHomePage(){
+        return "index";
     }
 }
