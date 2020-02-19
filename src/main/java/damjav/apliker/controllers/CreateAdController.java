92c261e3cccd558129de14bbf306b90130c45cb5
DamJav <damian.garbacz.9@gmail.com>
2020-02-19 20:40:43 +0100

    Struktura projektu

--- src/main/java/damjav/apliker/controllers/CreateAdController.java
+++ src/main/java/damjav/apliker/controllers/CreateAdController.java
@@ -1,4 +1,10 @@
 package damjav.apliker.controllers;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
 public class CreateAdController {
 }
