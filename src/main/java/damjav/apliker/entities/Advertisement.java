2c85fde5456edd1d37ae80680310568c1468bf66
DamJav <damian.garbacz.9@gmail.com>
2020-02-20 10:30:52 +0100

    Poprawka encji

--- src/main/java/damjav/apliker/entities/Advertisement.java
+++ src/main/java/damjav/apliker/entities/Advertisement.java
@@ -1,11 +1,18 @@
 package damjav.apliker.entities;
 
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 import javax.persistence.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Entity
+@Getter
+@Setter
+@ToString
 @Table(name = "advertisements")
 public class Advertisement {
 
