7d1cf046ba5e23c58499ae380eca02619e3de184
DamJav <damian.garbacz.9@gmail.com>
2020-02-21 10:55:31 +0100

    Wyszukiwanie aplikacji cd - poprawki

--- src/main/java/damjav/apliker/repositories/AdvertisementRepository.java
+++ src/main/java/damjav/apliker/repositories/AdvertisementRepository.java
@@ -3,10 +3,14 @@ package damjav.apliker.repositories;
 
 import damjav.apliker.entities.Advertisement;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface AdvertisementRepository extends JpaRepository<Advertisement, Long> {
 
-    Advertisement findByCompany(String companyName);
+
+    List<Advertisement> findByCompanyStartingWith(String companyName);
 
 
 }
