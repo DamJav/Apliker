dcd432a686b6a45f14be911da21ba671b581bf48
DamJav <damian.garbacz.9@gmail.com>
2020-02-23 16:38:32 +0100

    Historia wszystkich aplikacji..

--- src/main/java/damjav/apliker/repositories/AdvertisementRepository.java
+++ src/main/java/damjav/apliker/repositories/AdvertisementRepository.java
@@ -5,12 +5,19 @@ import damjav.apliker.entities.Advertisement;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 public interface AdvertisementRepository extends JpaRepository<Advertisement, Long> {
 
 
     List<Advertisement> findByCompanyStartingWith(String companyName);
+
+
+    @Transactional
+    @Query(value = "select * from advertisements order by sent desc", nativeQuery = true)
+    List<Advertisement> findAllAdsByDate();
+
 
 
 }
